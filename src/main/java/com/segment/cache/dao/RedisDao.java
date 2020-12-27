1ba2c74758d135f9d550305ea166e399ce60fe50
sinhanurag <sinhanurag@hotmail.com>
2020-12-27 00:31:47 -0800

    Adding multi container config

--- src/main/java/com/segment/cache/dao/RedisDao.java
+++ src/main/java/com/segment/cache/dao/RedisDao.java
@@ -1,16 +1,26 @@
 package com.segment.cache.dao;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
+import org.slf4j.Logger;
 
 public class RedisDao {
     private static Jedis client;
+    private static Logger logger;
 
     RedisDao(String host, int port) {
         client = new Jedis(host, port,1800);
+        logger = LoggerFactory.getLogger(RedisDao.class);
     }
 
     public String getCacheNodeFromRedis(String key) {
-        String value = client.get(key);
-        return value;
+        try {
+            String value = client.get(key);
+            return value;
+        } catch (Exception ex) {
+            logger.error(ExceptionUtils.getStackTrace(ex));
+            throw ex;
+        }
     }
 }
